1bd20e5e1d0d7b2a9fe96d0ea761e26aeb48e7e9
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2024-08-22 23:23:19 +0300

    Upload new problem solving in loops.

--- Loops/00-Watermelon .cpp
+++ Loops/00-Watermelon .cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int main() {
     int watermelon;
-    cin >> watermelon ;
-    if(watermelon > 2){
-        if(watermelon % 2 == 0)
-            cout<<"Yes\n";
+    cin >> watermelon;
+    if (watermelon > 2) {
+        if (watermelon % 2 == 0)
+            cout << "Yes\n";
         else
-            cout<<"No\n";
-    } else
-        cout<<"No\n";
+            cout << "No\n";
+    }
+    else
+        cout << "Watermelon Less Than 2\n";
     return 0;
 }
