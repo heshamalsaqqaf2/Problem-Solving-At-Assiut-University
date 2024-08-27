55680e8c1dbee54e7b61562a8726bfae0255fffe
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2024-08-27 23:43:41 +0300

    Upload new problem solvingfor loops.

--- 02-Loops/11-Divisors.cpp
+++ 02-Loops/11-Divisors.cpp
@@ -17,6 +17,11 @@
               //! Note:
                 Divisor of Number is A number that divides the integer exactly (no remainder).
                 In other words the division works perfectly with no fractions or remainders involved.
+                Examples:
+                    - 3 is a divisor of 12, because 12 ÷ 3 = 4 exactly
+                    - 4 is a divisor of 12, because 12 ÷ 4 = 3 exactly.
+                    - 5 is not a divisor of 12, because 12 ÷ 5 = 2 with a remainder of 2.
+                a divisor is also a factor of the original integer.
  *
  * @copyright Copyright Programmer/Hesham (c) 2024.
  */
