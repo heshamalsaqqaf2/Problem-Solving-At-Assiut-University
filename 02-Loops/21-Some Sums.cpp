3252da96e22ea87d6e5e75cf0b35986f5b3eaadf
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2024-08-28 23:36:46 +0300

    Upload new problem solving for loops.

---
+++ 02-Loops/21-Some Sums.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\n\tHello, Start The Program.\n";
+    cout << "==============================================\n";
+
+    cout << "==============================================\n";
+    cout << "\tGoodbye, End The Program.\n";
+    return 0;
+}
