870998a5a5da9b2a624b1699794569fb8103de2f
Hesham Alsaqqaf <heshamalsaqqaf14@gmail.com>
2024-08-29 00:45:39 +0300

    Upload new problem solving for loops.

--- 02-Loops/21-Some Sums.cpp
+++ 02-Loops/21-Some Sums.cpp
@@ -9,6 +9,21 @@ int main() {
     cout << "Enter 3 Numbers = ";
     cin >> number >> fromNumber >> inNumber;
 
+    int sumDigits{ 0 };
+    for (int i = 1; i <= number; ++i) {
+        int sumLastDigit{ 0 };
+        int counter = i;
+        while (counter > 0) {
+            sumLastDigit += counter % 10;
+            counter /= 10;
+        }
+        if (fromNumber <= sumLastDigit && sumLastDigit <= inNumber) {
+            sumDigits += i;
+            // cout << i << " ";
+        }
+    }
+    cout << "Summation = " << sumDigits << "\n";
+
     cout << "==============================================\n";
     cout << "\tGoodbye, End The Program.\n";
     return 0;
